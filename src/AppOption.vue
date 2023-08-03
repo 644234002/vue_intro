8cbf11df441ee6240652d3ec003c8a4b5b5f616b
Faqih <644234002@parichat.skru.ac.th>
2023-08-03 10:56:28 +0700

    3/8/66

--- src/AppOption.vue
+++ src/AppOption.vue
@@ -51,7 +51,45 @@
   </div>
 </template>
 
-<script setup>
+<script>
+export default {
+  //name : 'App',
+  data(){
+    return {
+    product: 'หูฟังไร้สาย Marshall Major IV',
+    brand: 'Marshall',
+    image: './images/major4.webp',
+    description: 'หูฟังจาก Marshall ในรุ่น Major IV เป็นหูฟังไร้สายชนิด on ear รุ่นที่ 4 แล้วครับ โดยในรุ่นนี้จะออกแบบให้สวมใส่สบายมากขึ้น พร้อมแบตเตอรี่ที่สามารถใช้งานได้นานสูงสุดถึง 80 ชั่วโมงเลยทีเดียว',
+    url: 'https://www.marshallheadphones.com',
+    inStock: true,
+    inventory: 20,
+    details: ['Wired', 'Wireless'],
+    variants: [
+        {id: 239, color: 'Black', image: './images/major4.webp'},
+        {id: 240, color: 'Brown', image: './images/major4.jpg'},
+    ],
+cart: 0
+}
+  },
+methods: {
+  //เพิ่มสินค้าลงตะกร้า
+  addToCart(){
+    this.cart += 1
+    this.inventory -= 1
+
+    //console.log(this.inventory)
+  },
+  updateImage(variantImage) {
+      this.image = variantImage
+    },
+},
+
+computed:{
+  title() {
+    return this.brand + ' ' + this.product + ' (' + this.inventory + ')'
+  }
+},
+}
 
 </script>
 
